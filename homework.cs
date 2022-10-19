b4f065e8b531b24b35a9ecde45f47533b1fc57f0
Kotikov <11deadsluts@gmail.com>
2022-10-19 23:03:54 +0300

    first task is solved

--- homework.cs
+++ homework.cs
@@ -1,1 +1,16 @@
 ﻿//Здраствуйте, Татьяна :)
+
+//Задача 1 (10): Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+int SecondNumber(int x)
+{
+    int digit1 = x%100;
+    int digit2 = digit1/10;
+    int result = digit2;
+    return result;
+}
+Console.WriteLine ("Input three-digit number, please");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = SecondNumber(num);
+Console.WriteLine ($"You typed {num} and your second digit is {result} ");
+*/
